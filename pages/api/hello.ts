fccd6d7becdb147b65a3510c5c2eb25e83cbc18b
John Carlo Austria <johncarloaustria@proton.me>
2022-08-31 00:41:05 +0800

    Fix hello api to be compatible with vercel edge
    Signed-off-by: John Carlo Austria <johncarloaustria@proton.me>

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import { type NextApiRequest, type NextApiResponse } from "next";
-
 export const config = {
 	runtime: "experimental-edge",
 };
 
-type Data = {
-	name: string;
-};
-
-export default function handler(_req: NextApiRequest, res: NextApiResponse<Data>) {
-	res.status(200).json({ name: "John Doe" });
+export default function handler() {
+	return new Response(
+		JSON.stringify({
+			message: "Hello!",
+		}),
+		{
+			status: 200,
+			headers: {
+				"content-type": "application/json",
+			},
+		},
+	);
 }
